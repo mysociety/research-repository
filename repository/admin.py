3621580999fd0f647f8de0b486f8df38640a0823
Alex Parsons <alex@alexparsons.co.uk>
2017-07-03 17:31:32 +0100

    Added django import export to admin files.

--- repository/admin.py
+++ repository/admin.py
@@ -1,26 +1,23 @@
 from django.contrib import admin
 from repository import models
+from import_export.admin import ImportExportModelAdmin 
+from useful_inkleby.useful_django.admin import io_admin_register
 
-
-class PersonAdmin(admin.ModelAdmin):
+@io_admin_register(models.Person)
+class PersonAdmin(ImportExportModelAdmin):
     list_display = ('full_name', 'institution')
     prepopulated_fields = {'slug': ('first_name', 'last_name')}
-
-admin.site.register(models.Person, PersonAdmin)
 
 
 class ItemAuthorInline(admin.TabularInline):
     model = models.ItemAuthor
     extra = 1
 
-
 class ResearchOutputInline(admin.TabularInline):
     model = models.ResearchOutput
 
-
-class ResearchItemAdmin(admin.ModelAdmin):
+@io_admin_register(models.ResearchItem)
+class ResearchItemAdmin(ImportExportModelAdmin):
     list_display = ('title', 'friendly_date', 'author_list')
     prepopulated_fields = {'slug': ('title',)}
     inlines = (ItemAuthorInline, ResearchOutputInline)
-
-admin.site.register(models.ResearchItem, ResearchItemAdmin)
