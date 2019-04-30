c92b042a3f44891fa4075a0d108622e8cadebedf
Alex <alex@alexparsons.co.uk>
2019-04-30 09:16:35 +0100

    Formatting fix for context_processors file

--- repository/context_processors.py
+++ repository/context_processors.py
@@ -2,28 +2,30 @@ from django.conf import settings
 from pages.models import Page, Link
 from repository.models import Tag, Site
 
+
 def get_pages_for_nav():
     pages = Page.objects.all().exclude(slug="home")
     pages = pages.exclude(nav_order=-1)
     pages = pages.order_by('nav_order')
     return pages
 
-def add_settings( request ):
+
+def add_settings(request):
     """Add some selected settings values to the context"""
-    
+
     site = Site.get_default()
-    
+
     return {
-        'top_links':Link.objects.all().exclude(order=-1).order_by('order'),
-        'all_pages':get_pages_for_nav(),
+        'top_links': Link.objects.all().exclude(order=-1).order_by('order'),
+        'all_pages': get_pages_for_nav(),
         'top_tags': Tag.objects.all().exclude(top_bar=-1).order_by('top_bar'),
-        'site':site,
+        'site': site,
         'settings': {
             'GOOGLE_ANALYTICS_ACCOUNT': settings.GOOGLE_ANALYTICS_ACCOUNT,
             'DEBUG': settings.DEBUG,
             'DEFAULT_SHARE_IMAGE': settings.DEFAULT_SHARE_IMAGE,
             'SITE_NAME': site.site_title,
-            'SITE_TWITTER_HANDLE': site.twitter,            
-            
+            'SITE_TWITTER_HANDLE': site.twitter,
+
         }
     }
