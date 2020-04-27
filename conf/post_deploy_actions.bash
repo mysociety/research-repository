f3edb2abe2c0df32a78780a96102e8d0893829b7
Alex <alex@alexparsons.co.uk>
2020-04-27 11:05:05 +0100

    Update packages and deploy scripts to python3

--- conf/post_deploy_actions.bash
+++ conf/post_deploy_actions.bash
@@ -15,26 +15,23 @@ virtualenv_dir='../virtualenv-repository'
 virtualenv_activate="$virtualenv_dir/bin/activate"
 if [ ! -f "$virtualenv_activate" ]
 then
-    virtualenv $virtualenv_args $virtualenv_dir
+    python3 -m venv $virtualenv_dir  --without-pip
 fi
 source $virtualenv_activate
 
 # Upgrade pip to a secure version
-pip_version="$(pip --version)"
-if [ "$(echo -e 'pip 1.4\n'"$pip_version" | sort -V | head -1)" = "$pip_version" ]; then
-    curl -L -s https://bootstrap.pypa.io/get-pip.py | python
-fi
+curl -L -s https://bootstrap.pypa.io/get-pip.py | python3
 
-pip install --requirement requirements.txt
+pip3 install --requirement requirements.txt
 
 # make sure that there is no old code (the .py files may have been git deleted)
 find . -name '*.pyc' -delete
 
 # get the database up to speed
-./manage.py migrate
+python3 manage.py migrate
 
 # gather all the static files in one place
-./manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # researchsites. This feels like a bit of a hack.
 researchsites_dir='../sites'
